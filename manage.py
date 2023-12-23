fef663cab4f2537c1aab0ed499a2e52b7508c7db
ukena18 <92485052+ukena18@users.noreply.github.com>
2023-12-23 08:13:32 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,6 +8,8 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restApp.settings')
     try:
+
+
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
